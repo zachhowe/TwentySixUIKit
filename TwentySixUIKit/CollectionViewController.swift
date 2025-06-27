0c95c060002c809af26ec7db7ef90774645b9c65
Zach Howe <zach.howe@tubi.tv>
2025-06-26 23:14:50 -0700

    Initial Commit

---
+++ TwentySixUIKit/CollectionViewController.swift
@@ -0,0 +1,173 @@
+//
+//  CollectionViewController.swift
+//  TwentySixUIKit
+//
+//  Created by Zach Howe on 6/26/25.
+//
+
+import UIKit
+
+@Observable
+class CellViewModel {
+    var text: String
+
+    init(_ text: String = "") {
+        self.text = text
+    }
+}
+
+class Cell: UICollectionViewCell {
+
+    var titleLabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+enum CollectionCellState {
+    case collapsed, expanded
+
+    var height: CGFloat {
+        switch self {
+        case .collapsed:
+            return 50
+        case .expanded:
+            return 200
+        }
+    }
+}
+
+@Observable
+class CollectionViewModel {
+
+    var cellState: CollectionCellState = .collapsed
+
+    var cellViewModels: [CellViewModel] = [
+        .init("😂"),
+        .init("😏"),
+        .init("😎"),
+        .init("😩"),
+        .init("🤖"),
+        .init("😴"),
+        .init("🙉"),
+        .init("😽"),
+        .init("😇"),
+        .init("🥸"),
+        .init("🤪"),
+        .init("🦒"),
+        .init("🦈"),
+        .init("🦷"),
+        .init("🍓"),
+        .init("🛹"),
+        .init("🚘"),
+        .init("⌚️"),
+        .init("🪰"),
+        .init("🐸"),
+        .init("🪴"),
+        .init("🌽"),
+        .init("🧇"),
+        .init("🐥"),
+        .init("🪼"),
+    ]
+}
+
+class CollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    var viewModel: CollectionViewModel
+
+    lazy var collectionView =  {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        return collectionView
+    }()
+
+    init(viewModel: CollectionViewModel = .init()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        title = "Collection View"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        view = collectionView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.register(Cell.self, forCellWithReuseIdentifier: "Cell")
+    }
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel.cellViewModels.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cellViewModel = viewModel.cellViewModels[indexPath.item]
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? Cell else {
+            fatalError("Expected to be able to dequeue `Cell` subclass")
+        }
+        cell.contentView.backgroundColor = .systemGray6
+        cell.configurationUpdateHandler = { [weak cell] _, state in
+            cell?.titleLabel.text = cellViewModel.text
+        }
+        return cell
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        let cellState = viewModel.cellState
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.itemSize = CGSize(width: collectionView.bounds.width, height: cellState.height)
+        }
+    }
+
+    override func updateProperties() {
+        super.updateProperties()
+
+        let expandOrCollapseCellButton = viewModel.cellState == .collapsed 
+            ? UIBarButtonItem(title: "Expand", style: .plain, target: self, action: #selector(expandOrCollapseCells))
+            : UIBarButtonItem(title: "Collapse", style: .plain, target: self, action: #selector(expandOrCollapseCells))
+
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(title: "Update", style: .plain, target: self, action: #selector(updateCells)),
+            expandOrCollapseCellButton,
+        ]
+    }
+
+    @objc private func updateCells() {
+        var i = 0
+        for cellViewModel in viewModel.cellViewModels {
+            cellViewModel.text += "\(i) "
+            i += 1
+        }
+    }
+
+    @objc private func expandOrCollapseCells() {
+        if viewModel.cellState == .collapsed {
+            viewModel.cellState = .expanded
+        } else {
+            viewModel.cellState = .collapsed
+        }
+    }
+}
---
+++ TwentySixUIKit/CounterViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  TwentySixUIKit
+//
+//  Created by Zach Howe on 6/26/25.
+//
+
+import UIKit
+
+extension CounterViewController {
+    @Observable
+    class ViewModel {
+        var count = 0
+        var textColor: UIColor = .darkText
+        var text: String = "Counter"
+    }
+}
+
+class CounterViewController: UIViewController {
+
+    let viewModel: ViewModel
+
+    lazy var stackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    lazy var incrementButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Increment", for: .normal)
+        return button
+    }()
+
+    lazy var decrementButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Decrement", for: .normal)
+        return button
+    }()
+
+    lazy var label = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 36)
+        return label
+    }()
+
+    init(viewModel: ViewModel = .init()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        title = "Counter"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stackView.addArrangedSubview(label)
+        stackView.addArrangedSubview(incrementButton)
+        stackView.addArrangedSubview(decrementButton)
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+        incrementButton.addTarget(self, action: #selector(onIncrementButtonAction), for: .primaryActionTriggered)
+        decrementButton.addTarget(self, action: #selector(onDecrementButtonAction), for: .primaryActionTriggered)
+    }
+
+    override func updateProperties() {
+        super.updateProperties()
+        label.textColor = viewModel.textColor
+        label.text = viewModel.text
+        tabBarItem.badgeValue = "\(viewModel.count)"
+    }
+
+    @objc private func onIncrementButtonAction() {
+        viewModel.count += 1
+        viewModel.text = "\(viewModel.count)"
+        viewModel.textColor = .green
+    }
+
+    @objc private func onDecrementButtonAction() {
+        viewModel.count -= 1
+        viewModel.text = "\(viewModel.count)"
+        viewModel.textColor = .red
+    }
+}
